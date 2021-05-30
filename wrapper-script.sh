05f20a774e069bcb4aff59ec0c7f7749e8cf45e8
prodigyoff <maryan.kit@gmail.com>
2021-05-30 19:04:25 +0300

    Test

--- wrapper-script.sh
+++ wrapper-script.sh
@@ -5,13 +5,14 @@ cd
 git clone -b osExam https://github.com/prodigyoff/dbLab4-6.git
 # shellcheck disable=SC2164
 cd dbLab4-6/
+export MYSQL_PWD=Pavlo?osada228
+/usr/bin/mysql -u root -e "CREATE DATABASE IF NOT EXISTS kit;"
+/usr/bin/mysql -u root -e "FLUSH PRIVILEGES;"
+/usr/bin/mysql -u root kit < src/main/resources/lab3DBScript.sql
 mvn install
 mvn clean package
 timeout 30s mvn spring-boot:run
 
 # shellcheck disable=SC2125
-export MYSQL_PWD=Pavlo?osada228
-/usr/bin/mysql -u root -e "CREATE DATABASE IF NOT EXISTS kit;"
-/usr/bin/mysql -u root -e "FLUSH PRIVILEGES;"
-/usr/bin/mysql -u root kit < src/main/resources/lab3DBScript.sql
-mvn spring-boot:run
+
+#mvn spring-boot:run
